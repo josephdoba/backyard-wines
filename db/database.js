0d36aead1aec19f4cb7a65d8d67f8702a5734ca3
Joseph Doba <josephdoba@gmail.com>
2022-06-27 18:03:03 -0700

    Connect jquery to contact.ejs. Move contact.js to /public/scripts. Basic empty-email check implemented

--- db/database.js
+++ db/database.js
@@ -3,16 +3,16 @@ const dbParams = require("../lib/db");
 const db = new Pool(dbParams);
 db.connect();
 
-const getAllWines = async () => {
+const getAllWines = async() => {
   const result = await db.query(`SELECT * FROM wine_listings`);
   console.log("result", result);
   return result.rows;
 };
-const getAllRedWines = async () => {
+const getAllRedWines = async() => {
   const result = await db.query(`SELECT * FROM wine_listings WHERE wine_type='Red'`);
   return result.rows;
 };
-const getAllWhiteWines = async () => {
+const getAllWhiteWines = async() => {
   const result = await db.query(`SELECT * FROM wine_listings WHERE wine_type='White'`);
   return result.rows;
 };
--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,19 +1,34 @@
 // User clicked on Send button:
 $(document).ready(function() {
-  console.log("contact ready!");
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+
+    // sanitize entry point:
+    let contactText = $(".contact-seller_message-area").val();
+    const data = $(this).serialize();
+    // const $emptyError = `You can't send an empty email!`; Stretch Feature
+    // const $errorIcon = `<i class="fa-solid fa-circle-exclamation"></i>`; Stretch Feature
+    // const $lengthError = `Email too long. Must be within 300 chars.`; Stretch Feature
+
+
+    // check if empty:
+    if (!contactText) {
+      alert("Cannot send a blank email");
+      // $(".new-tweet-error").slideDown(200, function() {
+      //   $(".new-tweet-error").toggleClass("new-tweet-error-border");
+      //   $(".new-tweet-error").text($emptyError);
+      //   $(".new-tweet-error").append($errorIcon);
+      //   $(".new-tweet-error").prepend($errorIcon);
+    }
+
+    // Send email:
+    console.log(contactText);
+
+  });
+
+
 });
-
-
-// $(document).ready(
-
-
-
-// $("form").on("submit", function(event) {
-//   event.preventDefault();
-//   console.log("Ayy the contact button is connected!");
-
-// })
-// );
 
 
 /*
