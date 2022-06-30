c17e9779daf94bcb162fd2f689f9cd84ccb6f754
Joseph Doba <josephdoba@gmail.com>
2022-06-29 22:04:14 -0700

    Finish favorite feature from all pages. Update favorites page accordingly on user account

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 // Client facing scripts here
 $(document).ready(function() {
   $('.wine-form').submit(function(e) {
-    let wineID = $(this).attr('data-id')
+    let wineID = $(this).attr('data-id');
     $.ajax({
       url: '/soldout',
       type: 'POST',
@@ -9,9 +9,9 @@ $(document).ready(function() {
         id: wineID
       }
     });
-  })
+  });
   $('.delete-form').submit(function(e) {
-    let wineID = $(this).attr('data-id')
+    let wineID = $(this).attr('data-id');
     $.ajax({
       url: '/removelisting',
       type: 'POST',
@@ -19,5 +19,19 @@ $(document).ready(function() {
         id: wineID
       }
     });
-  })
-})
+  });
+
+  $('.add-to-favorites-btn').submit(function(e) {
+    e.preventDefault();
+    let wineID = $(this).attr('data-id');
+    console.log(`this is wineID: ${wineID}`);
+    $.ajax({
+      url: '/api/favorite-item',
+      type: 'POST',
+      data: {
+        id: wineID
+      }
+    });
+  });
+
+});
